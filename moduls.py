de3e96a9500c3ab3092b028dabd806bcf699718e
Ари <arianna2811@yandex.ru>
2025-07-04 17:27:45 +0300

    feat: add check_input function

--- moduls.py
+++ moduls.py
@@ -1,1 +1,4 @@
-
+def check_input(data, type):
+    if not isinstance(data, type): raise TypeError("введите правильный тип данных")
+    if isinstance(type, (int, float)):
+        if data<=0 or data>=2025: raise ValueError("число должно быть валидным")
