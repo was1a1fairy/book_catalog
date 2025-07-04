83b73e500a445e4be7126a1add46062dff283ed6
Ари <arianna2811@yandex.ru>
2025-07-04 18:36:32 +0300

    feat: add book_catalog.txt and function catalog_to_matrix

--- moduls.py
+++ moduls.py
@@ -2,3 +2,10 @@ def check_input(data, type):
     if not isinstance(data, type): raise TypeError("введите правильный тип данных")
     if isinstance(type, (int, float)):
         if data<=0 or data>=2025: raise ValueError("число должно быть валидным")
+
+def catalog_to_matrix():
+    file = open("book_catalog.txt", "r")
+    matrix = []
+    for string in file.readlines():
+        matrix.append(string.split(","))
+    return matrix
